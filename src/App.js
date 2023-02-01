bc41f7608927c05d68a174bd3cee4b2fede2f502
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-01 16:35:19 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+
 
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
