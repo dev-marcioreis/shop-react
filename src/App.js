6d2daa683dd50453ceee9982cb27ed453c675ece
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 15:36:58 -0300

    notfound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
+import NotFound from './components/notFound/NotFound';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/shop-react' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import './notFound.css'
+
+const NotFound = () => {
+  return (
+    <>
+        <div className="section notfound">
+            <div className="container">
+                <h1>404 NotFound</h1>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default NotFound
