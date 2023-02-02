7318ae54a8f0f11900f58831349968760aa8e475
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 16:14:24 -0300

    correção de links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,20 +14,20 @@ const Header = () => {
         <header className="header">
           <div className="container flex2">
           <div className="header__logo">
-              <Link to='/'><h2 className='logo'>shop</h2></Link>
+              <Link to='/shop-react/'><h2 className='logo'>shop</h2></Link>
             </div>
             <ul className={mobileMenu ?  'header__list flex1' : 'link flex2'} onClick={() => setMobileMenu(false)}>
               <li className='header__link'>
-                <Link to='/'><i><Home /></i>home</Link>
+                <Link to='/shop-react/'><i><Home /></i>home</Link>
               </li>
               <li className='header__link'>
-                <Link to='/'><i><About /></i>sobre nós</Link>
+                <Link to='/shop-react/'><i><About /></i>sobre nós</Link>
               </li>
               <li className='header__link'>
-                <Link to='/'><i><Categ /></i>categorias</Link>
+                <Link to='/shop-react/'><i><Categ /></i>categorias</Link>
               </li>
               <li className='header__link'>
-                <Link to='/'><i><Contac /></i>fale conosco</Link>
+                <Link to='/shop-react/'><i><Contac /></i>fale conosco</Link>
               </li>
             </ul>
               <div className="header__cart">
