2d0cccb1accfae1f10a6b324b21abd7fbd27d4c2
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 20:14:41 -0300

    correção de link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="container footer__container">
             <article>
                 <div className="footer__logo">
-                  <Link to='/shop-react/'><h2 className='logo'>shop</h2></Link>
+                  <Link to='/shop-react'><h2 className='logo'>shop</h2></Link>
                 </div>
                 <p>As melhores marcas e os melhores produtos.</p>
                 <div className="footer__social">
@@ -20,24 +20,24 @@ const Footer = () => {
             </article>
             <article>
                 <h4>Institucional</h4>
-                <Link to='/shop-react/' className='footer__link'>Home</Link>
-                <Link to='/shop-react/' className='footer__link'>Sobre Nós</Link>
-                <Link to='/shop-react/' className='footer__link'>Categorias</Link>
-                <Link to='/shop-react/' className='footer__link'>Fale Conosco</Link>
+                <Link to='/shop-react' className='footer__link'>Home</Link>
+                <Link to='/shop-react' className='footer__link'>Sobre Nós</Link>
+                <Link to='/shop-react' className='footer__link'>Categorias</Link>
+                <Link to='/shop-react' className='footer__link'>Fale Conosco</Link>
             </article>
             <article>
                 <h4>Suporte</h4>
-                <Link to='/shop-react/' className='footer__link'>Pedidos</Link>
-                <Link to='/shop-react/' className='footer__link'>Entrega</Link>
-                <Link to='/shop-react/' className='footer__link'>Devolução</Link>
-                <Link to='/shop-react/' className='footer__link'>Cancelamento</Link>
+                <Link to='/shop-react' className='footer__link'>Pedidos</Link>
+                <Link to='/shop-react' className='footer__link'>Entrega</Link>
+                <Link to='/shop-react' className='footer__link'>Devolução</Link>
+                <Link to='/shop-react' className='footer__link'>Cancelamento</Link>
             </article>
             <article>
                 <h4>Corporativo</h4>
-                <Link to='/shop-react/' className='footer__link'>FAQ</Link>
-                <Link to='/shop-react/' className='footer__link'>Imprensa</Link>
-                <Link to='/shop-react/' className='footer__link'>Mpa do site</Link>
-                <Link to='/shop-react/' className='footer__link'>Trabalhe Conosco</Link>
+                <Link to='/shop-react' className='footer__link'>FAQ</Link>
+                <Link to='/shop-react' className='footer__link'>Imprensa</Link>
+                <Link to='/shop-react' className='footer__link'>Mpa do site</Link>
+                <Link to='/shop-react' className='footer__link'>Trabalhe Conosco</Link>
             </article>
         </div>
         <div className="footer__copy">
--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -11,7 +11,7 @@ const NotFound = () => {
                 <h2>Passei a vida tentando corrigir os erros que cometi na minha ânsia de acertar.</h2>
                 <i className='heart'><Heart /></i>
                 <p>página não encontrada!</p>
-                <Link to='/shop-react/' className='back-home'>
+                <Link to='/shop-react' className='back-home'>
                     <i><FcHome /></i>
                     <span>volte para a casinha</span>
                 </Link>
