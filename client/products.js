bc7bd452a9c9a7b0176a738290d0463659b9a9a5
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 17:04:25 -0300

    api de produtos

---
+++ client/products.js
@@ -0,0 +1,101 @@
+const products = [
+    {
+        id: 1,
+        name: 'Watch Series 4 GPS',
+        brand: 'Apple',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 399,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/apple-watch_prbuju.png'
+      },
+      {
+        id: 2,
+        name: 'Speaker',
+        brand: 'JBL',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 199,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/jbl-speaker_daa86d.png'
+      },
+      {
+        id: 3,
+        name: 'iPhone X 128GB',
+        brand: 'Apple',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 899,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/iphone-x_cx6bfh.png'
+      },
+      {
+        id: 4,
+        name: 'Headphones',
+        brand: 'Beats',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 459,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/beats-headphones_xppphj.png'
+      },
+      {
+        id: 5,
+        name: 'Mackbook Pro',
+        brand: 'Apple',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 2.499',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/macbook-pro_yj35uc.png'
+      },
+      {
+        id: 6,
+        name: 'iPad Pro 64GB',
+        brand: 'Apple',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 899,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/ipad-pro_zur4qd.png'
+      },
+      {
+        id: 7,
+        name: 'Homepod',
+        brand: 'Apple',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 399,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/homepod_mtdzpq.png'
+      },
+      {
+        id: 8,
+        name: 'Air Wireless',
+        brand: 'JBuds',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 249,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675171530/mks/jlab-audio-wireless_fg1szm.png'
+      },
+      {
+        id: 9,
+        name: 'Gopro Hero 11 Black',
+        brand: 'Gopro',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 3.239,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675367610/mks/gopro_nkviwe.png'
+      },
+      {
+        id: 10,
+        name: 'Drone Profissional E99 2022',
+        brand: 'E99',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 349,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675367804/mks/drone_m5p7lf.png'
+      },
+      {
+        id: 11,
+        name: 'Controle Dualsense Dragon Ball',
+        brand: 'Ps5',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 399,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675368006/mks/Dualsense-Dragon-Ball-2_d7ohrk.png'
+      },
+      {
+        id: 12,
+        name: 'Cadeira gamer profissional',
+        brand: 'Dark Flash',
+        description: 'Redesigned from scratch and completely revised.',
+        price: 'R$ 990,00',
+        image: 'https://res.cloudinary.com/dpuikgstw/image/upload/v1675368180/mks/cadeira-gamer_o1chuu.png'
+      },
+
+]
+
+module.exports = products
