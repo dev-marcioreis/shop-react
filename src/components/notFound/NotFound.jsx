1cbb7ba516787bc35eaa716fa685fcbd30d6282f
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 16:11:09 -0300

    notfound page finalizada

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,11 +1,20 @@
 import './notFound.css'
+import { Link } from 'react-router-dom'
+import { FcHome } from 'react-icons/fc'
+import { GiChewedHeart as Heart } from 'react-icons/gi'
 
 const NotFound = () => {
   return (
     <>
         <div className="section notfound">
-            <div className="container">
-                <h1>404 NotFound</h1>
+            <div className="container flex2">
+                <h2>Passei a vida tentando corrigir os erros que cometi na minha ânsia de acertar.</h2>
+                <i className='heart'><Heart /></i>
+                <p>página não encontrada!</p>
+                <Link to='/shop-react/' className='back-home'>
+                    <i><FcHome /></i>
+                    <span>volte para a casinha</span>
+                </Link>
             </div>
         </div>
     </>
