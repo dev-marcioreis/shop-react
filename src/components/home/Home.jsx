ec4c811779a7914d0fbc14422958c52f55651e83
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-02 11:45:13 -0300

    new component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,17 @@
 import Banner from '../banner/Banner'
+import NewsLetter from '../newsLetter/NewsLetter'
 import './home.css'
 
 const Home = () => {
   return (
     <>
-        <section className="home section">
-          <div className="container">
-            <Banner />
+        <section className='home'>
+          <div className="section">
+            <div className="container">
+              <Banner />
+            </div>
           </div>
+          <NewsLetter />
         </section>
     </>
   )
