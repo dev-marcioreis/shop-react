72ac0bfb25567c1ff5a8bf1cfb8a444cfbe58fb8
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-03 13:22:37 -0300

    cart empty pronto

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,11 +1,38 @@
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import './cart.css'
+import { FcHome as Home } from 'react-icons/fc'
+import { MdOutlineRemoveShoppingCart as Empty } from 'react-icons/md'
 
 const Cart = () => {
+
+  const cart = useSelector(state => state.cart)
+
   return (
     <>
         <section className="section cart">
           <div className="container">
-            <h1>Cart</h1>
+            <h2>carrinho</h2>
+            {
+              cart.cartItems.length === 0 ? (
+                <div className="cart__empty">
+                  <p>Seu carrinho está vazio.</p>
+                  <i className='empty__logo'><Empty /></i>
+                    <Link to='/shop-react' className="home__shop">
+                      <i><Home /></i>
+                      <span>voltar para comprar</span>
+                    </Link>
+                </div>
+              ) : (
+              <>
+                <div className="cart__titles flex2">
+                  <h3>item</h3>
+                  <h3>valor</h3>
+                  <h3>quantidade</h3>
+                  <h3>total</h3>
+                </div>
+              </>)
+            }
           </div>
         </section>
     </>
